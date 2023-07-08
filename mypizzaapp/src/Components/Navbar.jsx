fe99202679a1fa10aa7709e2fc2908a3492d5a3e
Omesh Dubey <105977022+omesh96@users.noreply.github.com>
2023-07-09 00:53:35 +0530

    assignment-completed

--- mypizzaapp/src/Components/Navbar.jsx
+++ mypizzaapp/src/Components/Navbar.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import "../Css/Navbar.css"
-import { FiAlignRight,FiXCircle,FiChevronDown } from "react-icons/fi"
-import {Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Image, Input, Spacer, useDisclosure} from "@chakra-ui/react"
-import { HamburgerIcon } from '@chakra-ui/icons'
+
 const Navbar = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = React.useRef()
+   
   return (
    <>
      <div className='largeScreen'>
@@ -90,102 +87,7 @@ shopping_cart
      </div>
 
 
-     {/* <div className='mobileScreen'>
-     <img src="https://radiustheme.com/demo/wordpress/themes/panpie/wp-content/themes/panpie/assets/img/logo-dark.png" alt="" />
-      
-      <li>
-        <span class="material-symbols-outlined icons">
-shopping_cart
-</span>
-        </li>
-        <li>
-        <span className="material-symbols-outlined icons">
-phone_in_talk
-</span>
-        </li>
-        <li>
-            <span class="material-symbols-outlined icons">search</span>
-            </li>
-            <span class="material-symbols-outlined">
-menu
-</span>
-     </div> */}
-
-{/* <div className='drawer'>
-  
-  <Box className='drawer-btn'>
-  <Button ref={btnRef} colorScheme='transparent' onClick={onOpen} >
-   <HamburgerIcon style={{ color:"black",height:"60px",width:"30px"}} />
-    </Button>
-  </Box>
-     <Box className='drawer-logo'>
-     <Image src="https://images.indianexpress.com/2021/01/myntra.png" alt="logo" s/>
     
-     </Box>
-         
-         <Box id='drawer-flex'>
-         <Box className="flex" >
-    < >
-    <Image src="https://cdn.icon-icons.com/icons2/2406/PNG/512/user_account_icon_145918.png" alt="acntlogo" 
-     style={{height:"25px"}}
-    />
-   
-    </>
-   </Box>
-
-   <Spacer />
-
-<Box className={"flex"} >
- < >
- <Image src="https://w7.pngwing.com/pngs/978/882/png-transparent-wishlist-save-save-for-later-shopping-ecommerce-favorite-add-to-wishlist-ecommerce-online-shopping-icon-thumbnail.png" alt="acntlogo" 
-  style={{height:"25px"}}
- />
- <Spacer />
-
- </>
-</Box>
-
-<Spacer />
-
-<Box className={"flex"} >
- < >
- <Image src="https://w7.pngwing.com/pngs/833/426/png-transparent-shopping-cart-icon-shopping-cart-black-design-trade-thumbnail.png" alt="acntlogo" 
-  style={{height:"25px"}}
- />
- <Spacer />
-
- </>
-</Box>
-         </Box>
-
-
-
-       <Drawer
-        isOpen={isOpen}
-        placement='right'
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <Box >
-  <Image src='https://i0.wp.com/fashionmostwanted.com/wp-content/uploads/2022/02/0c3b9-myntra-logo.jpeg?fit=1200%2C706&ssl=1' alt='Dan Abramov'  />
-</Box>
-
-          <DrawerBody>
-            <Input placeholder='Type here...' />
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-   </div> */}
    
    </>
   )
